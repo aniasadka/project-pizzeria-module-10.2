157aebf617f3168a82b870a80c1622948de73c39
Anna Sadka <anna.sadka87@gmail.com>
2020-09-02 20:57:30 +0200

    task 11.2 beginning

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,12 +1,16 @@
 import {
   select,
-  templates
+  templates,
+  settings
 } from '../settings.js';
+import {
+  utils
+} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
-class Booking {
+export class Booking {
   constructor(element) {
     const thisBooking = this;
 
@@ -15,43 +19,105 @@ class Booking {
     thisBooking.render(element);
     // wywoływać metodę initWidgets bez argumentów.
     thisBooking.initWidgets();
+    thisBooking.getData();
   }
 
-  render(element) {
+  getData() {
     const thisBooking = this;
 
-    //Metoda Booking.render ma za zadanie:
-    //generować kod HTML za pomocą szablonu templates.bookingWidget bez podawanie mu jakiegokolwiek argumentu,
-    const generatedHTML = templates.bookingWidget();
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
 
-    //tworzyć pusty obiekt thisBooking.dom,
-    thisBooking.dom = {};
-    //zapisywać do tego obiektu właściwość wrapper równą otrzymanemu argumentowi,
-    thisBooking.dom.wrapper = element;
-    //zawartość wrappera zamieniać na kod HTML wygenerowany z szablonu,
-    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
 
-    //we właściwości thisBooking.dom.peopleAmount zapisywać pojedynczy element znaleziony we wrapperze i pasujący do selektora select.booking.peopleAmount,
-    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
-    // analogicznie do peopleAmount znaleźć i zapisać element dla hoursAmount.
-    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+      eventsCurrent: [
+        settings.db.noRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
 
-    // W klasie Booking w metodzie render stwórz właściwość thisBooking.dom.datePicker i zapisz w niej element pasujący do selektora zapisanego w select.widgets.datePicker.wrapper, wyszukany we wrapperze zapisanym w tej klasie
-    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
-    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
-  }
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
 
-  initWidgets() {
-    const thisBooking = this;
+    //console.log('getData params', params);
 
-    // we właściwościach thisBooking.peopleAmount i thisBooking.hoursAmount zapisywać nowe instancje klasy AmountWidget, którym jako argument przekazujemy odpowiednie właściwości z obiektu thisBooking.dom.
-    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
-    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
 
-    // w metodzie initWidgets stwórz nową instancję klasy DatePicker zapisując ją do właściwości thisBooking.datePicker, analogicznie jak zrobiliśmy to dla obu instancji AmountWidget.
-    thisBooking.datepicker = new DatePicker(thisBooking.dom.datePicker);
-    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat.join('&'),
+    };
+
+    //console.log('getData urls', urls);
+
+    Promise.all([
+        fetch(urls.booking),
+        fetch(urls.eventsCurrent),
+        fetch(urls.eventsRepeat),
+      ])
+
+      .then(function (allResponses) {
+        const bookingResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
 }
+
+
+
+render(element) {
+  const thisBooking = this;
+
+  //Metoda Booking.render ma za zadanie:
+  //generować kod HTML za pomocą szablonu templates.bookingWidget bez podawanie mu jakiegokolwiek argumentu,
+  const generatedHTML = templates.bookingWidget();
+
+  //tworzyć pusty obiekt thisBooking.dom,
+  thisBooking.dom = {};
+  //zapisywać do tego obiektu właściwość wrapper równą otrzymanemu argumentowi,
+  thisBooking.dom.wrapper = element;
+  //zawartość wrappera zamieniać na kod HTML wygenerowany z szablonu,
+  thisBooking.dom.wrapper.innerHTML = generatedHTML;
+
+  //we właściwości thisBooking.dom.peopleAmount zapisywać pojedynczy element znaleziony we wrapperze i pasujący do selektora select.booking.peopleAmount,
+  thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+  // analogicznie do peopleAmount znaleźć i zapisać element dla hoursAmount.
+  thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+  // W klasie Booking w metodzie render stwórz właściwość thisBooking.dom.datePicker i zapisz w niej element pasujący do selektora zapisanego w select.widgets.datePicker.wrapper, wyszukany we wrapperze zapisanym w tej klasie
+  thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+  thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+}
+
+initWidgets() {
+  const thisBooking = this;
+
+  // we właściwościach thisBooking.peopleAmount i thisBooking.hoursAmount zapisywać nowe instancje klasy AmountWidget, którym jako argument przekazujemy odpowiednie właściwości z obiektu thisBooking.dom.
+  thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+  thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+  // w metodzie initWidgets stwórz nową instancję klasy DatePicker zapisując ją do właściwości thisBooking.datePicker, analogicznie jak zrobiliśmy to dla obu instancji AmountWidget.
+  thisBooking.datepicker = new DatePicker(thisBooking.dom.datePicker);
+  thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+}
+
 
 export default Booking;
